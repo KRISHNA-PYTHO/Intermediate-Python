dc69a9434ea175c1076e99ec8990f278752d296f
Krishna mourya <maurya1389@gmail.com>
2025-01-28 23:14:16 +0530

    update

---
+++ 13. Polymorphism/13. Polymorphism concept.py
@@ -0,0 +1,29 @@
+class Animal:
+    def feed(self):
+        pass 
+
+class Lion(Animal):
+    def feed(self):
+        print("Feeding meat to the lion")
+
+class Elephant(Animal):
+    def feed(self):
+        print("Feeding plants to the elephant")
+
+class Bird(Animal):
+    def feed(self):
+        print("Feeding seeds to the bird")
+
+def perform_feeding(animal):
+    animal.feed()
+
+# Usage
+lion = Lion()
+elephant = Elephant()
+bird = Bird()
+
+perform_feeding(lion)       #  Feeding meat to the lion
+perform_feeding(elephant)   #  Feeding plants to the elephant
+perform_feeding(bird)       #  Feeding seeds to the bird
+
+
---
+++ 13. Polymorphism/17. hassttr( ) function.py
@@ -0,0 +1,32 @@
+class Dog:
+    def speak(self):
+        return "Woof!"
+   
+class Cat:
+    def speak(self):
+        return "Meow!"
+
+class Duck:
+    def run(self):
+        return "Slow"
+
+def make_sound(animal):
+
+    #print(animal.speak())
+
+    if hasattr(animal,'speak'):
+         print(animal.speak())
+    
+    if hasattr(animal,'run'):
+         print(animal.run())
+
+# Instances of different classes
+dog = Dog()
+cat = Cat()
+duck = Duck()
+
+# Calling the function with different objects
+make_sound(dog)  # Output: Woof!
+make_sound(cat)  # Output: Meow!
+make_sound(duck)  # Output: Quack!
+
